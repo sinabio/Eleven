3734c458e53e52ef728e7d9c30b27d7dcc98b99a
coderyi <coderyi@foxmail.com>
2015-08-26 17:32:13 +0800

    eleven video player bug

--- Eleven/Vendor/kxmovie/KxLogger.h
+++ Eleven/Vendor/kxmovie/KxLogger.h
@@ -12,20 +12,20 @@
 //#define DUMP_AUDIO_DATA
 
 #ifdef DEBUG
-//#ifdef USE_NSLOGGER
-//
-//#    import "NSLogger.h"
-//#    define LoggerStream(level, ...)   LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"Stream", level, __VA_ARGS__)
-//#    define LoggerVideo(level, ...)    LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"Video",  level, __VA_ARGS__)
-//#    define LoggerAudio(level, ...)    LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"Audio",  level, __VA_ARGS__)
-//
-//#else
+#ifdef USE_NSLOGGER
+
+#    import "NSLogger.h"
+#    define LoggerStream(level, ...)   LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"Stream", level, __VA_ARGS__)
+#    define LoggerVideo(level, ...)    LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"Video",  level, __VA_ARGS__)
+#    define LoggerAudio(level, ...)    LogMessageF(__FILE__, __LINE__, __FUNCTION__, @"Audio",  level, __VA_ARGS__)
+
+#else
 
 #    define LoggerStream(level, ...)   NSLog(__VA_ARGS__)
 #    define LoggerVideo(level, ...)    NSLog(__VA_ARGS__)
 #    define LoggerAudio(level, ...)    NSLog(__VA_ARGS__)
 
-//#endif
+#endif
 #else
 
 #    define LoggerStream(...)          while(0) {}
